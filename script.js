1d85ae2cf2f475ae75f843f76a5a640c45c36c2f
Manzoor Sofi <sofimanzoor888@gmail.com>
2023-08-26 19:47:25 +0530

    backgrounds

--- script.js
+++ script.js
@@ -28,14 +28,14 @@ $(document).ready(function () {
 
 	// typing text animation script
 	var typed = new Typed(".typing", {
-		strings: ["Developer", "Designer", "Freelancer"],
+		strings: ["Developer", "Freelancer"],
 		typeSpeed: 100,
 		backSpeed: 60,
 		loop: true,
 	});
 
 	var typed = new Typed(".typing-2", {
-		strings: ["Developer", "Designer", "Freelancer"],
+		strings: ["Developer", "Freelancer"],
 		typeSpeed: 100,
 		backSpeed: 60,
 		loop: true,
